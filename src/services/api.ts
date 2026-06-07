81fc50faa102c94485b1c030665374494d275e7c
NikCodePro <vermanikhil7838@gmail.com>
2026-06-08 00:27:34 +0530

    going live

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 // API Base Configuration
-// export const API_BASE_URL = "https://api.sankatseva.com/api/v1";
-export const API_BASE_URL = "http://192.168.1.16:8000/api/v1";
+export const API_BASE_URL = "https://api.sankatseva.com/api/v1";
+// export const API_BASE_URL = "http://192.168.1.16:8000/api/v1";
 
 export type ApiResponse<T> = {
   success: boolean;
